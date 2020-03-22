cf8519bfc23f7720acf660625434a3a5983d7083
Vladyslav Bilous <vladyslav.bilous23@gmail.com>
2020-03-22 03:46:01 +0100

    added main page + serch form

--- pks/src/app/app.module.ts
+++ pks/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import {SearchFormComponent} from './search-form/search-form.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DropDownsModule,
+    BrowserAnimationsModule,
+    DropDownsModule,
+    InputsModule,
+    FormsModule,
+    InputsModule,
+    ReactiveFormsModule,
+    DateInputsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
