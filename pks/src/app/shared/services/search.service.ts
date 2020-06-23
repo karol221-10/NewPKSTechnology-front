456102fbcfc690d7012f3307c9d6ac07c546aba4
Karol Ślusarz <karol221-10@hotmail.com>
2020-06-23 17:16:57 +0200

    Update date format.

--- pks/src/app/shared/services/search.service.ts
+++ pks/src/app/shared/services/search.service.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs';
 import {Track} from '../../main-page/search-form/track.model';
 import {BaseApi} from '../core/base-api';
 import {City} from '../../system/shared/models/city.model';
+import {formatDate} from '@angular/common';
 
 
 @Injectable()
@@ -13,19 +14,22 @@ export class SearchService extends BaseApi {
   }
 
   getTrack(form): Observable<any> {
+    debugger;
     const formatD = this.updateData(form.data).toJSON().slice(0, 10);
-    const formatFrom = this.updateData(form.from).toJSON().slice(10, 24);
-    const formatTo = this.updateData(form.to).toJSON().slice(10, 24);
+    const formatFrom = form.from.toJSON().slice(10, 24);
+    const formatTo = form.to.toJSON().slice(10, 24);
     // const timeFrom = (form.from.toTimeString()).slice(0, 5);
     // const timeTo = (form.to.toTimeString()).slice(0, 5);
-    const startTime = formatD + formatFrom;
-    const endTime = formatD + formatTo;
+    const startTime = new Date(formatD + formatFrom);
+    const endTime = new Date(formatD + formatTo);
+    const startTimeString = formatDate(startTime, 'yyyyMMddHHmmss', 'en-US');
+    const endTimeString = formatDate(endTime, 'yyyyMMddHHmmss', 'en-US');
     console.log('form:', form);
     console.log(startTime);
     console.log(endTime);
     console.log(form.leavingFrom.value);
     // tslint:disable-next-line:max-line-length
-    return this.getTest(`schedule?sourceTownId=${form.leavingFrom.value}&destinationTownId=${form.leavingTo.value}&startTime=${startTime}&endTime=${endTime}`);
+    return this.getTest(`schedule?sourceTownId=${form.leavingFrom.value}&destinationTownId=${form.leavingTo.value}&startTime=${startTimeString}&endTime=${endTimeString}`);
   }
 
   getTrackList(): Observable<any[]> {
@@ -44,13 +48,13 @@ export class SearchService extends BaseApi {
   postTicket(data) {
     const body = JSON.stringify(data);
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this.httpClient.post('http://www.kompikownia.pl:4444/api/ticket', body, {headers});
+    return this.httpClient.post('http://localhost:4444/api/ticket', body, {headers});
   }
 
   setTicket(data) {
     const body = JSON.stringify(data);
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this.httpClient.post('http://www.kompikownia.pl:4444/api/ticket/complete', body , {headers});
+    return this.httpClient.post('http://localhost:4444/api/ticket/complete', body , {headers});
   }
 
 
